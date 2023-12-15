6c2bd59db48118d1e952ff75e58add2c25a14dd3
정도현 <cirel31@gmail.com>
2023-12-15 20:46:17 +0900

    column annotation 공부

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserModel } from './entity/user.entity';
+import { Repository } from 'typeorm';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  constructor(
+    @InjectRepository(UserModel)
+    private readonly userRepository: Repository<UserModel>,
+  ) {}
+  @Post('users')
+  postUser() {
+    return this.userRepository.save({ title: 'test' });
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import {TypeOrmModule} from '@nestjs/typeorm'
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModel } from './entity/user.entity';
 @Module({
   imports: [
+    TypeOrmModule.forFeature([UserModel]),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: '127.0.0.1',
@@ -11,9 +13,9 @@ import {TypeOrmModule} from '@nestjs/typeorm'
       username: 'postgres',
       password: 'postgres',
       database: 'typeormstudy',
-      entities: [],
+      entities: [UserModel],
       synchronize: true,
-    })
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
